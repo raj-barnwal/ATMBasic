be31a23f3459a795c61a3a18f157ed14153c5dbc
Ankur Ttnd <ankur@tothenew.com>
2017-03-07 13:36:56 +0530

    Add money to ATM functionality added

--- func.js
+++ func.js
@@ -18,6 +18,36 @@ function Atm(){
 var atm = new Atm();
 //console.log(atm);
 
+//Add money to Atm
+function addMoney(noof2000, noof500, noof100, total_money){
+  atm.amount += total_money;
+  atm.avlNotes["2000"] += noof2000;
+  atm.avlNotes["500"] += noof500;
+  atm.avlNotes["100"] += noof100;
+}
+
+//Get values from user
+function addNotes()
+{
+	var noof2000=parseInt(document.getElementById('no2000').value);
+	var noof500=parseInt(document.getElementById('no500').value);
+	var noof100=parseInt(document.getElementById('no100').value);
+  if(noof2000 < 0 || noof500 < 0 || noof100 < 0){
+    alert("Please enter a valid value!!!!");
+    return;
+  }
+	//var total_notes_added=noof2000+noof500+noof100;
+	var total_money=(noof2000*2000 + noof500*500 + noof100*100);
+  if(total_money){
+    alert("Please add something!!")
+    return;
+  }
+
+  addMoney(noof2000, noof500, noof100, total_money);
+
+	alert("Total Money Added : " + total_money + "\nNumber of 2000 notes : " + noof2000 + "\n" + "Number of 500 notes : " + noof500+ "\nNumber of 100 notes : " + noof100);
+}
+
 
 //Validates that the amount can be withdrawl or not.
 function validate(amountTransacted){
